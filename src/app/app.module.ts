af3b20dda807274aabb9c6b1f1150e3f5530e05d
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2023-02-09 19:01:13 -0600

    Images lazy loading is applied

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatGridListModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    NgOptimizedImage,
   ],
   providers: [],
   bootstrap: [AppComponent],
